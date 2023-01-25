3c97cb5a668b9764da4f4da090bf2a8b6729b13a
Tuomas Huhtala <tuomas.huhtala@icloud.com>
2023-01-25 15:08:57 +0200

    part1.2

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,22 +1,25 @@
-const Header = (props) => (
-    <h1>
-      {props.course}
-    </h1>
-  )
+const Header = (props) => {
+  console.log(props)
+  return <h1>{props.course}</h1>
+}
 
-const Part = (props) => (
+const Part = (props) => {
+    return (
     <p>
       {props.part} {props.exercises}
     </p>
   )
+}
 
-const Content = (props) => (
+const Content = (props) => {
+  return (
     <div>
       <Part part={props.parts[0].part} exercises={props.parts[0].exercises} />
       <Part part={props.parts[1].part} exercises={props.parts[1].exercises} />
       <Part part={props.parts[2].part} exercises={props.parts[2].exercises} />
     </div>
   )
+}
 
 function Total(props) {
   return (
